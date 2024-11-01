ac6c08c7614d7802cb4dce3f01befe2f5f8dfeb6
gkvartskhava <gio.kvartskhava@gmail.com>
2024-11-01 14:02:43 +0200

    initial setup

---
+++ courier/urls.py
@@ -0,0 +1,35 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+         title = 'Courier Delivery Service API',
+         default_version = "V1",
+         description =  "API for managing courier deriveries"
+    ),
+    public= True,
+    permission_classes=(AllowAny,),
+)
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name = 'schema-swagger-ui'),
+    path('api-auth/', include('rest_framework.urls', namespace= "rest_framework")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+]
